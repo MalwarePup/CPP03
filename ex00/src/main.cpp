ca0d77c00ffa6f7cbf586e9f8b92fc946f58240e
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-11-01 08:01:41 +0100

    feat: add ClapTrap class implementation with basic functionality
    Add a Makefile to automate the build process for the ClapTrap project.
    Introduce ClapTrap class with constructors, destructor, and methods for
    attacking, taking damage, and repairing. Implement main.cpp to
    demonstrate the usage of ClapTrap class with various actions.

---
+++ ex00/src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "ClapTrap.hpp"
+
+int main()
+{
+	ClapTrap defaultClap;
+	ClapTrap clappy("Clappy");
+	ClapTrap copyClap(clappy);
+	ClapTrap assignedClap;
+	assignedClap = clappy;
+
+	std::cout << "\033[1;34m\n--- Actions for Clappy ---\033[0m" << std::endl;
+	clappy.attack("target1");
+	clappy.takeDamage(4);
+	clappy.beRepaired(3);
+	clappy.attack("target2");
+	clappy.takeDamage(12);
+	clappy.beRepaired(5);
+
+	std::cout << "\033[1;34m\n--- Actions for Default Clap ---\033[0m" << std::endl;
+	defaultClap.attack("target3");
+	defaultClap.takeDamage(6);
+	defaultClap.beRepaired(7);
+	defaultClap.attack("target4");
+	defaultClap.takeDamage(15);
+
+	std::cout << "\033[1;34m\n--- Actions for CopyClap (copy of Clappy) ---\033[0m" << std::endl;
+	copyClap.attack("target5");
+	copyClap.takeDamage(3);
+	copyClap.beRepaired(2);
+
+	std::cout << "\033[1;34m\n--- Actions for AssignedClap (assigned from Clappy) ---\033[0m" << std::endl;
+	assignedClap.attack("target6");
+	assignedClap.takeDamage(2);
+	assignedClap.beRepaired(5);
+	assignedClap.attack("target7");
+	std::cout << std::endl;
+
+	return 0;
+}
